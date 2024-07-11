96d25192a40840f577be7f6913e165aa18b6261e
DankAlighieri <guilhermeemeterioif@gmail.com>
2024-07-11 11:08:12 -0300

    Google ReCAPTCHA frontend integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { log } from "console";
 import { FormEvent, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha"
 
 export default function Home() {
 
@@ -8,6 +10,7 @@ export default function Home() {
   const [email, setEmail] = useState("");
   const [telefone, setTelefone] = useState("");
   const [mensagem, setMensagem] = useState("");
+  const [captcha, setCaptcha] = useState<string | null>()
 
     const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
@@ -20,17 +23,18 @@ export default function Home() {
       };
 
       try {
-        const rawReponse = await fetch("http://localhost:3000/api", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
-        console.log(rawReponse);
-        const content = await rawReponse.json();
-        console.log(content);
+        if(captcha) {
+          const rawReponse = await fetch("http://localhost:3000/api", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+          });
+        } else {
+          alert("Invalid ReCAPTCHA!")
+        }
 
         setNome("");
         setEmail("");
@@ -42,6 +46,7 @@ export default function Home() {
         alert("There was an error submitting the form. Please try again.");
       }
     };
+
 
   return (
     <main className="bg-gray-100 min-h-screen">
@@ -85,11 +90,18 @@ export default function Home() {
                     id="mensagem" 
                     className="shadow-md focus:ring-indigo-500 focus:border-indigo-500 block w-64 sm:text-md border-gray-300 rounded-md text-black" placeholder="Sua Mensagem" />
                 </div>
+                <ReCAPTCHA
+                  className="flex items-center justify-center"
+                  sitekey = {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+                  onChange={setCaptcha}        
+                ></ReCAPTCHA>
                 <div className="flex items-center justify-center">
                     <button type="submit" className="flex items-center justify-center text-sm w-64 rounded-md shadow py-3 px-2 text-white bg-indigo-600 text-black">Enviar</button>
                 </div>
             </form>
+            
         </div>
+      
     </main>
   )
 }
